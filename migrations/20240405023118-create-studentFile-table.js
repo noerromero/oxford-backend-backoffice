3c33954cf567ec648453a6785d086e6d280afdec
Noe Romero <noerromero@gmail.com>
2024-04-06 23:43:01 -0400

    Add student file table migration

---
+++ migrations/20240405023118-create-studentFile-table.js
@@ -0,0 +1,41 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable(
+      "studentFiles",
+      {
+        id: {
+          type: Sequelize.STRING,
+          primaryKey: true,
+          allowNull: false,
+          unique: true,
+        },
+        student_id: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        academic_institution: {
+          type: Sequelize.STRING,
+          allowNull: true,
+        },
+        workplace: {
+          type: Sequelize.STRING,
+          allowNull: true,
+        },
+        english_certificate: {
+          type: Sequelize.STRING(100),
+          allowNull: true,
+        },
+      },
+      {
+        schema: "backoffice",
+      }
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("backoffice.studentFiles");
+  },
+};
