0b7b1033317d24c4a8f7ed77416028136fc01190
Noe Romero <noerromero@gmail.com>
2024-04-06 23:42:40 -0400

    Add db schema

--- migrations/20240404021955-create-student-table.js
+++ migrations/20240404021955-create-student-table.js
@@ -3,49 +3,55 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("students", {
-      id: {
-        type: Sequelize.STRING,
-        primaryKey: true,
-        allowNull: false,
-        unique: true,
+    await queryInterface.createTable(
+      "students",
+      {
+        id: {
+          type: Sequelize.STRING,
+          primaryKey: true,
+          allowNull: false,
+          unique: true,
+        },
+        dni: {
+          type: Sequelize.STRING(8),
+          allowNull: false,
+        },
+        name: {
+          type: Sequelize.STRING(100),
+          allowNull: false,
+        },
+        surname: {
+          type: Sequelize.STRING(100),
+          allowNull: false,
+        },
+        second_surname: {
+          type: Sequelize.STRING(100),
+          allowNull: true,
+        },
+        email: {
+          type: Sequelize.STRING,
+          allowNull: true,
+        },
+        phone: {
+          type: Sequelize.STRING(15),
+          allowNull: true,
+        },
+        bhirtdate: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+        cellphone: {
+          type: Sequelize.STRING(15),
+          allowNull: true,
+        },
       },
-      dni: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      name: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      surname: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      second_surname: {
-        type: Sequelize.STRING,
-        allowNull: true,
-      },
-      email: {
-        type: Sequelize.STRING,
-        allowNull: true,
-      },
-      phone: {
-        type: Sequelize.STRING,
-        allowNull: true,
-      },
-      bhirtdate: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
-      cellphone: {
-        type: Sequelize.STRING,
-        allowNull: true,
-      },
-    });
+      {
+        schema: "backoffice",
+      }
+    );
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("students");
+    await queryInterface.dropTable("backoffice.students");
   },
 };
