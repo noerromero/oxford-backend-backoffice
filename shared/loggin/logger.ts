4c62d7eb190c6af6df53cf885d0a70a9367aa3b2
Noe Romero <noerromero@gmail.com>
2024-05-01 08:51:31 -0400

    Add date to file log name

--- shared/loggin/logger.ts
+++ shared/loggin/logger.ts
@@ -1,11 +1,16 @@
 import { createLogger, transports, format } from "winston";
 
+const date = new Date();
+const year = date.getFullYear();
+const month = `0${date.getMonth() + 1}`.slice(-2);
+const day = `0${date.getDate()}`.slice(-2);
+
 export const logger = createLogger({
   transports: [
     new transports.File({
-      filename: "logs/error.log",
+      filename: `logs/error${year}${month}${day}.log`,
       level: "error",
       format: format.combine(format.timestamp(), format.json()),
-    })
+    }),
   ],
 });
