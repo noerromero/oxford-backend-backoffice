cf2fdf3162be0e590f11eabca997b95a373d113d
noerromero <noerromero@gmail.com>
2024-08-23 21:27:45 -0400

    Add isNotEmpty function

--- src/Shared/Infrastructure/Utils/StringUtils.ts
+++ src/Shared/Infrastructure/Utils/StringUtils.ts
@@ -2,4 +2,8 @@ export class StringUtils {
     static isEmpty(value: string) {
         return value === null || value === undefined || value === "";
     }
+
+    static isNotEmpty(value: string) {
+        return !StringUtils.isEmpty(value);
+    }
 }
